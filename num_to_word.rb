028ee780f90b0dfa37b5c98243682c40a520b99d
john <jayelwhy@yahoo.com>
2015-05-25 15:56:15 -0700

    number to word

---
+++ num_to_word.rb
@@ -0,0 +1,58 @@
+class Fixnum
+
+  define_method(:to_words) do
+    digits = { 0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine",
+      10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen" }
+      tens = { 2 => "twenty", 3 => "thirty", 4 => "fourty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninty" }
+      hundred = ["hundred", "thousand", "million", "billion", "trillion"]
+      writen = []
+      if self.<=19
+        digits.fetch(self)
+      else
+        numbers = self.to_s.split('').map(&:to_i)
+        numbers.reverse!()
+        if numbers.length==2
+           writen.push(tens.fetch(numbers[1]))
+           writen.push(digits.fetch(numbers[0]))
+
+        elsif numbers.length==3
+            writen.push(digits.fetch(numbers[2]))
+            writen.push(hundred[0])
+            writen.push(tens.fetch(numbers[1]))
+            writen.push(digits.fetch(numbers[0]))
+
+        elsif numbers.length==4
+            writen.push(digits.fetch(numbers[3]))
+            writen.push(hundred[1])
+            writen.push(digits.fetch(numbers[2]))
+            writen.push(hundred[0])
+            writen.push(tens.fetch(numbers[1]))
+            writen.push(digits.fetch(numbers[0]))
+
+
+          elsif numbers.length==5
+            if numbers[4]==1
+              teen = []
+              teen.push(numbers[4])
+              teen.push(numbers[3])
+              teens = teen.join()
+              teens = teens.to_i
+              writen.push(digits.fetch(teens))
+              writen.push(hundred[1])
+              writen.push(digits.fetch(numbers[2]))
+              writen.push(hundred[0])
+              writen.push(tens.fetch(numbers[1]))
+              writen.push(digits.fetch(numbers[0]))
+            else
+              writen.push(tens.fetch(numbers[4]))
+              writen.push(digits.fetch(numbers[3]))
+              writen.push(hundred[1])
+              writen.push(digits.fetch(numbers[2]))
+              writen.push(hundred[0])
+              writen.push(tens.fetch(numbers[1]))
+              writen.push(digits.fetch(numbers[0]))
+         end
+
+      end
+  end
+end
